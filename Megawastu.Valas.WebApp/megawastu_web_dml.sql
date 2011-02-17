9f804c527697925af1bcfbf506b5f4de67f3f5dd
unknown <jogisilalahi@.(none)>
2011-02-17 21:24:16 +0700

    update for ddl dan dml sql

---
+++ Megawastu.Valas.WebApp/megawastu_web_dml.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `mwp_news` VALUES (1, 'testing aplikasi update', 'coba ini adalah isi dari content yang baru di update', 1, '2011-01-25 21:19:30', 'guest');
+INSERT INTO `mwp_news` VALUES (2, 'testing aplikasi yang kedua untuk news', 'testing aplikasi yang kedua untuk news', 2, '2011-01-25 21:33:28', 'guest');
+
+
+INSERT INTO `mwp_users` VALUES (3, 'jogi', '21232f297a57a5a743894a0e4a801fc3', 1);
