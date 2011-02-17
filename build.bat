4ae4a7bc98770c288a482fb8ad98eee958801362
welly87 <if05041@gmail.com>
2011-02-17 21:51:42 +0700

    copy web

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 rmdir /s /q installer\bin
 mkdir installer\bin
+mkdir installer\bin\megawastu
 cd Megawastu.Valas.RateFeeder
 call build.bat create.installer
 cd ..\Megawastu.Valas.KursProvider
 call build.bat create.installer
+cd ..\Megawastu.Valas.WebApp
+xcopy /E src ..\installer\bin\megawastu
