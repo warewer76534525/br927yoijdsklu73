dbf6502e0558815b90a9147208895dbe44dfd59e
Adi Sembiring <sembiring.adi@gmail.com>
2011-02-18 23:05:36 +0700

    update ddl data

--- installer/sql/megawastu_web_ddl.sql
+++ installer/sql/megawastu_web_ddl.sql
@@ -20,7 +20,7 @@ CREATE TABLE `mwp_sessions` (
   `ip_address` varchar(16) NOT NULL default '0',
   `user_agent` varchar(50) NOT NULL,
   `last_activity` int(10) unsigned NOT NULL default '0',
-  `user_data` text NOT NULL,
+  `user_data` text NOT NULL DEFAULT '',
   PRIMARY KEY  (`session_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
