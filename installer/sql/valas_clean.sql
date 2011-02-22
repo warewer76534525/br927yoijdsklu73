3a7b0592208d29501b6aa0fd56d468475dfe129a
Adi Sembiring <sembiring.adi@gmail.com>
2011-02-22 07:05:59 +0700

    add mysql user script

--- installer/sql/valas_clean.sql
+++ installer/sql/valas_clean.sql
@@ -1,1 +1,24 @@
 DROP database IF EXISTS `valas`;
+
+-- drop database user
+USE `test` ;
+
+DROP PROCEDURE IF EXISTS `test`.`drop_user_if_exists` ;
+DELIMITER $$
+CREATE PROCEDURE `test`.`drop_user_if_exists`(IN p_username VARCHAR(255))
+BEGIN
+  DECLARE foo BIGINT DEFAULT 0 ;
+  SELECT COUNT(*)
+  INTO foo
+    FROM `mysql`.`user`
+      WHERE `User` =  p_username ;
+  
+  IF foo > 0 THEN 
+         DROP USER  'valas' ;
+  END IF;
+END ;$$
+DELIMITER ;
+
+CALL `test`.`drop_user_if_exists`('valas') ;
+
+DROP PROCEDURE IF EXISTS `test`.`drop_users_if_exists` ;
