a9197e817be2238ad5a3ca57a014274a9b09b814
welly87 <if05041@gmail.com>
2011-02-24 16:09:41 +0700

    removing duplicate sql dan merged

--- installer/sql/valas_user.sql
+++ installer/sql/valas_user.sql
@@ -1,7 +1,7 @@
 USE `test` ;
 
 DROP PROCEDURE IF EXISTS `test`.`add_user_if_exists` ;
-DELIMITER $$
+-- DELIMITER $$
 CREATE PROCEDURE `test`.`add_user_if_exists`(IN p_username VARCHAR(255))
 BEGIN
   DECLARE foo BIGINT DEFAULT 0 ;
